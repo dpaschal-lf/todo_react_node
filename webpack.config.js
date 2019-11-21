23884dce3bd08ce75bdcbe5bbbee4ea0b132de58
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-21 14:35:34 -0800

    initial intall and webpack file

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: path.join(__dirname, '/bundle'),
+		filename: 'index_bundle.js'
+	},
+	devServer: {
+		inline: true,
+		port: 3001,
+	},
+	module: {
+		rules: [
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015','react']
+			}
+		]
+	},
+	plugins: [
+		new HTMLWebpackPlugin({ template: './index.html'})
+	]
+}
