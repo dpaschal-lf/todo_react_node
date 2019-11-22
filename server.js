eba692b62cd83d5a0782b15e22f858448984d3db
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-22 15:47:21 -0800

    added basic todo item details

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ server.get('/api/items', (req,res)=>{
     res.send([{"title":"this is a test todo in mysql","added":"2019-05-02 00:00:00","id":"1","completed":"active"},{"title":"get eggs","added":"2019-07-02 00:00:00","id":"2","completed":"completed"},{"title":"test2","added":"2019-11-20 19:24:41","id":"3","completed":"completed"},{"title":"make an example","added":"2019-11-20 19:25:59","id":"4","completed":"completed"}]);
 })
 
+server.get('/api/items/:id', (req,res)=>{
+    res.send({"title":"get eggs","added":"2019-07-02 00:00:00","id":"2","completed":"completed","description":"the big eggs"});
+})
+
 server.listen(5000, ()=>{
     console.log('server operational');
 })
--- src/TodoDetails.js
+++ src/TodoDetails.js
@@ -5,11 +5,36 @@ class TodoDetails extends React.Component{
     constructor(props){
         super(props);
         this.state ={
-
+            data: {}
         }
     }
+    componentDidMount(){
+        fetch('http://localhost:5000/api/items/'+this.props.match.params.id)
+            .then( res => res.json() )
+            .then( data => {
+                this.setState({
+                    data
+                });
+            })
+    }
     render(){
-        return (<div>details</div>)
+        return (
+            <div className="details">
+                <div className="titleControls">
+        <div className="title">{this.state.data.title}</div>
+                    <div className="controls">
+                        <input value="completed" name="completed" className="completeCheckbox updatable" type="checkbox"/>
+                        <div className="delete button">Delete</div>
+                    </div>
+                </div>
+                <div name="description" className="description">{this.state.data.description}</div>
+                <input type="datetime-local" name="added" className="added" value={this.state.data.added}/>
+                <div className="updateControls hidden">
+                        <div className="saveButton button">save</div>
+                        <div className="cancelButton button">cancel</div>
+                </div>
+            </div>
+        );
     }
 }
 
