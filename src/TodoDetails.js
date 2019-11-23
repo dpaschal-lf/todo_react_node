6050fa6a42e580012e7b6c595fa42b76bef34ca7
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-23 15:12:50 -0800

    added modal to details

--- src/TodoDetails.js
+++ src/TodoDetails.js
@@ -5,8 +5,11 @@ import handleToken from './handleToken';
 class TodoDetails extends React.Component{
     constructor(props){
         super(props);
+        this.hideModal = this.hideModal.bind( this );
+        this.confirmDelete = this.confirmDelete.bind( this );
         this.state ={
-            data: {}
+            data: {},
+            modalMessage: null
         }
     }
     componentDidMount(){
@@ -25,6 +28,20 @@ class TodoDetails extends React.Component{
                     data
                 });
             })
+    }
+    hideModal(){
+        this.setState({
+            modalMessage: null
+        })
+    }
+    confirmDelete(){
+        this.setState({
+            modalMessage: <React.Fragment>
+                You are about to delete <em>{this.data.title}</em>
+                <p>Are you sure?</p>
+                <span className="button" onClick={this.handleDelete}>Delete</span><span onClick={this.hideModal} className="button">Cancel</span>
+            </React.Fragment>
+        })
     }
     handleDelete(){
         fetch('http://localhost:5000/api/items/'+this.props.match.params.id,{
@@ -46,7 +63,7 @@ class TodoDetails extends React.Component{
                     <div className="title">{this.state.data.title}</div>
                     <div className="controls">
                         <input value="completed" name="completed" className="completeCheckbox updatable" type="checkbox"/>
-                        <div className="delete button">Delete</div>
+                        <div className="delete button" onClick={this.confirmDelete}>Delete</div>
                     </div>
                 </div>
                 <div name="description" className="description">{this.state.data.description}</div>
