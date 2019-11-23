38237030dead096409333ef6a62e215df22d52ff
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-22 20:36:14 -0800

    changed to bodyparser.  is it gone again? also reverted all statusSend to status when chained with send

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const server = express();
 
 const staticModule = express.static(path.normalize(__dirname+'../public'));
-const bodyParser = express.bodyParser();
+//const bodyParser = express.bodyParser();
 
 server.use( staticModule );
 server.use( cors() );
@@ -18,7 +19,7 @@ const db = mysql.createConnection( mysqlCredentials );
 server.get('/api/items', (req,res)=>{
     db.connect( (error)=>{
         if(error){
-            res.sendStatus(500).send( error.message );
+            res.status(500).send( error.message );
             return;
         }
         const query = 'SELECT `title`, `added`, `id`, `completed` FROM `items`';
@@ -26,7 +27,7 @@ server.get('/api/items', (req,res)=>{
             if(!error){
                 res.send(data);
             } else {
-                res.sendStatus(500).send(error);
+                res.status(500).send(error);
             }
         });
     });
@@ -35,18 +36,18 @@ server.get('/api/items', (req,res)=>{
 server.get('/api/items/:id', (req,res)=>{
     db.connect( (error)=>{
         if(error){
-            res.sendStatus(500).send( error.message );
+            res.status(500).send( error.message );
             return;
         }
         if(isNaN(req.params.id)){
-            res.sendStatus(500).send( `id ${req.params.id} is not a number` );
+            res.status(500).send( `id ${req.params.id} is not a number` );
         }
         const query = 'SELECT `title`, `added`, `id`, `completed`, `description` FROM `items` WHERE `id`=?';
         db.query( query, [req.params.id], (error, data) =>{
             if(!error){
                 res.send(data);
             } else {
-                res.sendStatus(404).send(`cannot find record for id ${req.params.id}`)
+                res.status(404).send(`cannot find record for id ${req.params.id}`)
             }
         });
     });
@@ -55,30 +56,32 @@ server.get('/api/items/:id', (req,res)=>{
 server.delete('/api/items/:id', (req,res)=>{
     db.connect( (error)=>{
         if(error){
-            res.sendStatus(500).send( error.message );
+            res.status(500).send( error.message );
             return;
         }
         if(isNaN(req.params.id)){
-            res.sendStatus(500).send( `id ${req.params.id} is not a number` );
+            res.status(500).send( `id ${req.params.id} is not a number` );
         }
         const query = 'DELETE FROM `items` WHERE `id`=?';
         db.query( query, [req.params.id], (error) =>{
             console.log('delete: ',error);
             if(!error){
-                res.send(200);
+                res.sendState(200);
                 return;
             } 
-            res.send(500);
+            res.sendStatus(500);
         });
     });
 })
 
 server.post('/api/items/', (req,res)=>{
+    
     db.connect( (error)=>{
         const requiredFields = ['title','description'];
         for( let fieldIndex = 0; fieldIndex < requiredFields.length; fieldIndex++){
+            console.log(req.body);
             if(req.body[requiredFields[fieldIndex]]===undefined){
-                res.sendStatus(500).send(`${requiredFields[fieldIndex]} is required`);
+                res.status(500).send(`${requiredFields[fieldIndex]} is required`);
                 return;
             }
         }
@@ -87,10 +90,10 @@ server.post('/api/items/', (req,res)=>{
         db.query( query, [req.params.id], (error) =>{
             console.log('delete: ',error);
             if(!error){
-                res.send(200);
+                res.sendStatus(200);
                 return;
             } 
-            res.send(500);
+            res.sendStatus(500);
         });
     });
 })
