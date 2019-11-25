04859e718c3c104c93e3fe20efa828531b7e6c3a
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-25 10:21:54 -0800

    added callback handling in list item

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -3,13 +3,18 @@ import {Link} from 'react-router-dom';
 
 
 export default props => {
-    console.log(props);
+    const handleCompleteToggle = ( e )=>{
+        e.stopPropagation();
+        const element = e.target;
+        const checkedValue = element.checked ? 'checked' : 'active';
+        this.props.completeUpdateCallback( props.data.id, checkedValue );
+    }
     return (
         <Link to={`/details/${props.data.id}`} className="todoListItem row">
             <div className="title col-sm-5">{props.data.title}</div>
             <div className="added col-sm-5">{props.data.added}</div>
             <div className="controlArea col-sm-2">
-                <input type="checkbox" checked={props.data.completed ==='completed'?'checked':''}/>
+                <input onClick={handleCompleteToggle} type="checkbox" checked={props.data.completed ==='completed'?'checked':''}/>
             </div>
         </Link>
     )
