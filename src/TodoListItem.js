88b18bfed7b32ee8baabd357ebf8d67639486bd2
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-25 13:34:10 -0800

    used date format for todo list item

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {getMonth, getDayOfWeek, getOrdinal} from './dateConvert.js';
 
 
 export default props => {
@@ -9,10 +10,12 @@ export default props => {
         const checkedValue = element.checked ? 'completed' : 'active';
         props.completeUpdateCallback( props.data.id, checkedValue );
     }
+    const dateObject = new Date(props.data.added);
+    const formattedDate = `${getDayOfWeek(dateObject)}, ${getMonth(dateObject)} ${dateObject.getDate()}${getOrdinal(dateObject.getDate())}, ${dateObject.getFullYear()}`;
     return (
         <Link to={`/details/${props.data.id}`} className="todoListItem row">
             <div className="title col-sm-5">{props.data.title}</div>
-            <div className="added col-sm-5">{props.data.added}</div>
+            <div className="added col-sm-5">{formattedDate}</div>
             <div className="controlArea col-sm-2">
                 <input onClick={handleCompleteToggle} type="checkbox" checked={props.data.completed ==='completed'?'checked':''}/>
             </div>
