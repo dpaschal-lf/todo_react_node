aa0bc794be8fb72fdea9f9129e7e18b6df3b4d18
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-26 18:40:58 -0800

    build

---
+++ build/precache-manifest.78d0a7703219fed6c6f508933b56acc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c24dfc923532b5bd450da3585c9ca9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34996d2ad082481990f5",
+    "url": "/static/css/main.2fd19ca6.chunk.css"
+  },
+  {
+    "revision": "6352cb98e6bbfa20ada1",
+    "url": "/static/js/2.94bd5b40.chunk.js"
+  },
+  {
+    "revision": "34996d2ad082481990f5",
+    "url": "/static/js/main.32ce7447.chunk.js"
+  },
+  {
+    "revision": "6e8e4d45b64e0987fe10",
+    "url": "/static/js/runtime-main.869004dc.js"
+  }
+]);
